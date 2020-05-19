2c2a4312aa1ab346344ea4c26f5cec02f3b135d1
lyrav2 <lyrav2@gmail.com>
2020-05-19 06:43:30 -0400

    Testing changes

---
+++ script.js
@@ -0,0 +1,12 @@
+var label = "senecacollege";
+var tld = "ca";
+var domainName = label + "." + tld;
+console.log(domainName);
+
+var isSeneca;
+
+if (domainName == "senecacollege.ca") {
+    isSeneca === true;
+} else if (domainName != "senecacollege.ca") {
+    isSeneca === false;
+}
